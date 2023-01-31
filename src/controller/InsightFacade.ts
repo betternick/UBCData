0350e4288529e4602718541b988dadcf05409184
syedwaqarahmed <syedw@student.ubc.ca>
2023-01-30 21:29:23 -0800

    - Made new branch - Starting on addDataset function

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -18,6 +18,19 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+
+		// TO DO: CheckParams: Check if add dataset params (ID, content, kind) are acceptable
+
+		// TO DO: unZip: read zip file. check that not empty
+
+		// TO DO: makeDataset: add valid sections to array
+
+		// TO DO: createDataset: add the dataset created in prev step to insightFacade object. Update insight
+		// facade object with info about added dataset.
+
+		// TO DO: diskWrite: write this object to data folder for persistence.
+
+
 		return Promise.reject("Not implemented.");
 	}
 
