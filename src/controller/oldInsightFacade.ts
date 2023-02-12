201711c2ba5bb85266fa855ca330074744ec2e9e
Linda Kiritchkov <lindakiritchkov@gmail.com>
2023-02-12 10:20:04 -0800

    created insightfacade and querycontainer backup files

---
+++ src/controller/oldInsightFacade.ts
@@ -0,0 +1,204 @@
+import {
+	Dataset,
+	IInsightFacade,
+	InsightDataset,
+	InsightDatasetKind,
+	InsightError,
+	InsightResult,
+	NotFoundError,
+} from "./IInsightFacade";
+
+import {QueryContainer} from "../model/QueryContainer";
+
+import {
+	addToPersistFolder,
+	checkIdAndKind,
+	deleteDatasetFromPersistence,
+	readContent, readDirectory,
+} from "./helperFunctionsAddDataset";
+import {clearDisk, getContentFromArchives} from "../../test/TestUtil";
+import {expect} from "chai";
+
+/**
+ * This is the main programmatic entry point for the project.
+ * Method documentation is in IInsightFacade
+ *
+ */
+export default class InsightFacade implements IInsightFacade {
+	public static map: Map<string, Dataset>;
+
+	public static persistDir = "./data";
+	public static persistFile = "./data/persistFile.json";
+
+	constructor() {
+		InsightFacade.map = new Map<string, Dataset>();
+		console.log("InsightFacadeImpl::init()");
+		readDirectory(InsightFacade.persistDir, InsightFacade.map);
+	}
+
+	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+		return new Promise(function (resolve, reject) {
+			let keys: string[];
+			let throwFlag = 0;
+
+			let myDataset: Dataset = {
+				id: id,
+				kind: InsightDatasetKind.Sections,
+				numRows: 0,
+				datasetArray: [],
+			};
+
+			checkIdAndKind(id, kind, InsightFacade.map)
+				.then(() => {
+					return readContent(content, myDataset.datasetArray);
+				})
+
+				.then((length) => {
+					myDataset.numRows = length;
+					InsightFacade.map.set(id, myDataset);
+
+					// ref: https://linuxhint.com/convert-map-keys-to-array-javascript/
+					keys = [...InsightFacade.map.keys()];
+					// resolve(keys);
+					// Step 5): diskWrite: write this object to data folder for persistence.
+					return addToPersistFolder(myDataset, keys);
+				})
+				.then((keysParam) => {
+					resolve(keysParam);
+				})
+				.catch((err) => {
+					reject(new InsightError(err));
+				});
+		});
+	}
+
+	public removeDataset(id: string): Promise<string> {
+		return new Promise(function (resolve, reject) {
+			if (id.includes("_")) {
+				reject(new InsightError("_ character is not allowed"));
+			}
+			// Removing whitespace reference: https://stackoverflow.com/questions/10800355/remove-whitespaces-inside-a-string-in-javascript
+			if (id.replace(/\s+/g, "").length === 0) {
+				reject(new InsightError("ID cannot have only whitespaces"));
+			}
+
+			if (!InsightFacade.map.has(id)) {
+				console.log(InsightFacade.map.has(id));
+				reject(new NotFoundError("Dataset doesn't exist"));
+			}
+
+			InsightFacade.map.delete(id);
+
+			deleteDatasetFromPersistence(InsightFacade.persistDir, id)
+				.then(() => {
+					resolve(id);
+				})
+				.catch(() => {
+					reject(new InsightError("could not delete dataset"));
+				});
+		});
+	}
+
+	public performQuery(query: unknown): Promise<InsightResult[]> {
+		return new Promise((resolve, reject) => {
+			// Step 1) check if the query is a JSON object
+			if (typeof query !== "object") {
+				reject(new InsightError("Not a valid JSON object"));
+			} else if (query == null) {
+				reject(new InsightError("query is null?"));
+			} else {
+				let queryParsed = Object.entries(query); // queryParsed has type "Object"
+				let datasetID = this.getDatasetID(query);
+
+				let datasetToQuery = InsightFacade.map.get(datasetID);
+
+				if (datasetToQuery === undefined) {
+					reject(new InsightError("the dataset you are looking for has not been added"));
+				}
+
+				// catch first level of query (OPTIONS or WHERE)
+				if (Object.prototype.hasOwnProperty.call(query, "WHERE") ||
+					Object.prototype.hasOwnProperty.call(query, "OPTIONS")
+				) {
+					// check that query object has BOTH OPTIONS and WHERE
+					if (Object.prototype.hasOwnProperty.call(query, "WHERE") &&
+						Object.prototype.hasOwnProperty.call(query, "OPTIONS")
+					) {
+						let queryObject = new QueryContainer();
+
+						// handleOptions
+						try {
+							queryObject.handleOptions(queryParsed[this.getIndex(queryParsed, "OPTIONS")][1], datasetID);
+						} catch (error) {
+							reject(error);
+						}
+
+						// handleWhere
+						let results: InsightResult[] = [] ;
+						try {
+							results = queryObject.handleWhere(
+								queryParsed[this.getIndex(queryParsed, "WHERE")][1],
+								datasetID,
+								datasetToQuery as Dataset
+							);
+						} catch (error) {
+							reject(error);
+						}
+
+						// handleSort
+						results = queryObject.handleSort(results);
+						resolve(results);
+					}
+					reject(new InsightError("query missing WHERE or OPTIONS block"));
+				}
+			}
+			reject(new InsightError("query missing WHERE and OPTIONS blocks"));
+		});
+	}
+
+	public listDatasets(): Promise<InsightDataset[]> {
+		return new Promise(function (resolve, reject) {
+			let array: InsightDataset[] = [];
+			// ref: https://www.hackinbits.com/articles/js/how-to-iterate-a-map-in-javascript---map-part-2
+			InsightFacade.map.forEach(function (value, key) {
+				let thing: InsightDataset = {
+					id: "a",
+					kind: InsightDatasetKind.Sections,
+					numRows: 0,
+				};
+				thing.id = key;
+				thing.kind = value.kind;
+				thing.numRows = value.numRows;
+				array.push(thing);
+			});
+			resolve(array);
+		});
+	}
+
+	// ------------------  HELPER FUNCTIONS ------------------
+
+	// determines if the ID is valid
+	public datasetIDValid(datasetID: string): boolean {
+		return !datasetID.includes("_");
+	}
+
+	// finds the first dataset ID from a query
+	public getDatasetID(query: object): string {
+		const queryString = JSON.stringify(query);
+		const indexUnderscore = queryString.indexOf("_");
+		const indexStartOfID = queryString.lastIndexOf('"', indexUnderscore) + 1;
+		let result: string;
+		result = queryString.substring(indexStartOfID, indexUnderscore);
+		return result;
+	}
+
+	public getIndex(queryParsed: string | any, item: string): number {
+		let index: number = -1;
+		for (let x = 0; x < queryParsed.length; x++) {
+			if (queryParsed[x][0] === item) {
+				index = x;
+			}
+		}
+		return index;
+	}
+}
---
+++ src/model/oldQueryContainer.ts
@@ -0,0 +1,299 @@
+import {Dataset, InsightError, InsightResult, ResultTooLargeError} from "../controller/IInsightFacade";
+
+export class QueryContainer {
+	public columns: string[];
+	public order: string;
+
+	constructor() {
+		this.columns = [];
+		this.order = "";
+	}
+
+	// handles the WHERE block in a query
+	// throws InsightError("multiple datasets referenced") if any dataset ID's in WHERE block don't match
+	// otherwise, returns the InsightResult[] that corresponds to the query
+	public handleWhere(query: object, datasetID: string, dataset: Dataset): InsightResult[] {
+		let resultArray: InsightResult[] = [];
+		if (JSON.stringify(query) !== "{}") {
+			// WHERE block is not empty
+
+			// recursively traverse JSON query object:
+			// ref: https://blog.boot.dev/javascript/how-to-recursively-traverse-objects/
+			for (let queryKey in query) {
+				if (queryKey === "OR") {
+					for (let item in Object.values(query)[0]) {
+						let nextItem = Object.values(query)[0][item];
+						let arr = this.handleWhere(nextItem, datasetID, dataset);
+						resultArray = resultArray.concat(arr);
+					}
+				} else if (queryKey === "AND") {
+					for (let courseSection in dataset.datasetArray) {
+						// iterate through every course section in the dataset
+						let section = JSON.stringify(dataset.datasetArray[courseSection]);
+						// console.log("got to AND"); TODO: recurse, but keeping in mind the and structure
+						// pretty sure I do something like this:
+						// create # of arrays that match the total number of items in and
+						// for (item in AND)
+						// 		arr# = handlewhere(item)
+						// traverse arr1
+						// 		- for every item in arr 1, see if it's included in arr2/3/4...
+						//		- if yes, keep it. If not, remove it
+						//		- concat(arr1 with resultArray)
+					}
+				} else if (queryKey === "NOT") {
+					// console.log("got to NOT"); TODO: recurse, but keeping in mind the not structure
+					for (let courseSection in dataset.datasetArray) {
+						// iterate through every course section in the dataset
+						let section = JSON.stringify(dataset.datasetArray[courseSection]);
+					}
+				} else {
+					for (let courseSection in dataset.datasetArray) {
+						// iterate through every course section in the dataset
+						let section = JSON.stringify(dataset.datasetArray[courseSection]);
+						this.applyComparator(datasetID, query, section, resultArray, queryKey);
+					}
+				}
+			}
+		} else {
+			// WHERE body is empty -> return all entries -> create test case that doesn't return too many results
+			let courseResultArray = dataset.datasetArray[0];
+			for (let course in courseResultArray) {
+				// for each course in the dataset
+				for (let column in this.columns) {
+					// for each column option in the query
+				}
+			}
+		}
+		return resultArray;
+	}
+
+	private applyComparator(
+		datasetID: string,
+		query: object,
+		section: string,
+		resultArray: InsightResult[],
+		comparator: string
+	) {
+		this.singleDatasetID("", datasetID); // check that multiple datasets aren't referenced
+		let arr = Object.values(query);
+		let field = this.transformQueryToDatasetConvention(this.returnIdentifier(JSON.stringify(arr[0])));
+		let value = this.returnValueToSearch(JSON.stringify(arr[0]));
+		let valueType = this.returnValueType(field);
+		let match = this.doesThisSectionMatch(section, field, value, valueType, comparator);
+		if (match) {
+			let myInsightResult: InsightResult = {};
+			for (let col in this.columns) {
+				let keyCol = datasetID.concat("_", this.transformDatasetToQueryConvention(this.columns[col]));
+				let val = this.getValue(section, this.columns[col], this.returnValueType(this.columns[col]));
+				// To convert string to number: ref: https://stackoverflow.com/questions/23437476/in-
+				// typescript-how-to-check-if-a-string-is-numeric/23440948#23440948
+				let keyVal: string | number;
+				if (this.returnValueType(this.columns[col]) === "number") {
+					keyVal = Number(val);
+				} else {
+					keyVal = val;
+				}
+				if (keyCol === datasetID + "_year") {
+					// need to check if sections = overall, if yes, year = 1900
+					let sec = this.getValue(section, "Section", "string");
+					console.log("SECTION IS: " + sec);
+					if (sec === "overall") {
+						keyVal = 1900;
+					} else {
+						keyVal = Number(keyVal);
+					}
+				} else if (keyCol === datasetID + "_uuid") {
+					keyVal = keyVal.toString();
+				}
+				myInsightResult[keyCol] = keyVal;
+			}
+			resultArray.push(myInsightResult);
+			if (resultArray.length > 5000) {
+				throw new ResultTooLargeError("Exceeded 5000 entries");
+			}
+		}
+	}
+
+	// sorts the array based on this.order
+	public handleSort (array: InsightResult[]): InsightResult[] {
+		// sorting array of arrays by string property value, dynamically: ref: https://stackoverflow.com/questions/
+		// 1129216/sort-array-of-objects-by-string-property-value
+		function dynamicSort(property: string) {
+			let sortOrder = 1;
+			return function (a: InsightResult, b: InsightResult) {
+				let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+				return result * sortOrder;
+			};
+		}
+		return array.sort(dynamicSort(this.order));
+	}
+
+	public doesThisSectionMatch(section: string, field: string, value: string, valueType: string, comparator: string
+	): boolean {
+		let indexStartOfValue: number;
+		let indexEndOfValue: number;
+		if (valueType === "string") {
+			indexStartOfValue = section.indexOf(field) + field.length + 2;
+			indexEndOfValue = section.indexOf('"', indexStartOfValue + 1) + 1;
+		} else {
+			indexStartOfValue = section.indexOf(field) + field.length + 2;
+			indexEndOfValue = section.indexOf(",", indexStartOfValue);
+		}
+		let val: string = "";
+		if (field === "Year") {
+			// need to check if sections = overall, if yes, year = 1900
+			let sec = this.getValue(section, "Section", "string");
+			if (sec === "overall") {
+				val = "1900";
+			} else {
+				val = section.substring(indexStartOfValue + 1, indexEndOfValue - 1);
+			}
+		} else {
+			val = section.substring(indexStartOfValue, indexEndOfValue);
+		}
+		if (comparator === "EQ" || comparator === "IS") {
+			if (field === "id") {
+				val = "\"" + val + "\"";
+			}
+			return val === value;
+		} else if (comparator === "GT") {
+			return Number(val) > Number(value);
+		} else { // comparator === "LT"
+			return Number(val) < Number(value);
+		}
+	}
+
+	// handles the OPTIONS block in a query
+	// throws InsightError("multiple datasets referenced") if any dataset ID's
+	// found in the OPTIONS block do not match the datasetID parameter
+	public handleOptions(query: object, datasetID: string) {
+		let queryString: string = JSON.stringify(query);
+		this.singleDatasetID(queryString, datasetID);
+
+		// if there is an ORDER section, extract the order
+		if (queryString.includes("ORDER")) {
+			let indexOfOrderStart = queryString.indexOf("ORDER") + 8;
+			let indexOfOrderEnd = queryString.indexOf("\"", indexOfOrderStart);
+			this.order = queryString.substring(indexOfOrderStart, indexOfOrderEnd);
+		}
+		// creates a substring that contains only the columns
+		let indexOfColumnsStart = queryString.indexOf("[") + 2;
+		let indexOfColumnsEnd = queryString.indexOf("]");
+		let columnsString = queryString.substring(indexOfColumnsStart, indexOfColumnsEnd);
+
+		// extracts all the column identifiers and puts them into the columns array
+		while (columnsString.length !== 0) {
+			let indexStartOfNextColIden = columnsString.indexOf('"') + 3;
+			this.columns.push(this.returnIdentifier(columnsString));
+			columnsString = columnsString.substring(indexStartOfNextColIden);
+		}
+		this.columns.sort(); // sort columns alphabetically
+		for (let col in this.columns) {
+			this.columns[col] = this.transformQueryToDatasetConvention(this.columns[col]);
+		}
+	}
+
+	// checks whether only a single ID is referenced
+	public singleDatasetID(query: string, datasetID: string) {
+		for (let i = 0; i < query.length; i++) {
+			if (query[i] === "_") {
+				let indexStartOfID = query.lastIndexOf('"', i) + 1;
+				let result = query.substring(indexStartOfID, i);
+				if (result !== datasetID) {
+					throw new InsightError("multiple datasets referenced");
+				}
+			}
+		}
+		return;
+	}
+
+	// returns the identifier after an underscore
+	public returnIdentifier(query: string): string {
+		const indexUnderscore = query.indexOf("_");
+		const indexEndOfIdentifier = query.indexOf('"', indexUnderscore);
+		return query.substring(indexUnderscore + 1, indexEndOfIdentifier);
+	}
+
+	// return value used during the search (ie. value we are looking for)
+	public returnValueToSearch(query: string) {
+		const indexStartOfIdentifier = query.indexOf(":");
+		const indexEndOfIdentifier = query.indexOf("}", indexStartOfIdentifier);
+		return query.substring(indexStartOfIdentifier + 1, indexEndOfIdentifier);
+	}
+
+	// return value after identifier in a string
+	// Linda -> probably want to refactor returnValueToSearch and doesThisSectionMatch to just use this function instead
+	public getValue(str: string, identifier: string, type: string): string {
+		let indexStartOfValue = str.indexOf(identifier) + identifier.length + 2;
+		let indexEndOfValue: number;
+		if (type === "string") {
+			indexStartOfValue += 1;
+			indexEndOfValue = str.indexOf('"', indexStartOfValue);
+			return str.substring(indexStartOfValue, indexEndOfValue);
+		} else {
+			indexEndOfValue = str.indexOf(",", indexStartOfValue);
+			return str.substring(indexStartOfValue, indexEndOfValue);
+		}
+	}
+
+	// transform field from naming convention in query to naming convention in dataset
+	private transformQueryToDatasetConvention(field: string): string {
+		if (field === "uuid") {
+			field = "id";
+		} else if (field === "id") {
+			field = "Course";
+		} else if (field === "title") {
+			field = "Title";
+		} else if (field === "instructor") {
+			field = "Professor";
+		} else if (field === "dept") {
+			field = "Subject";
+		} else if (field === "year") {
+			field = "Year";
+		} else if (field === "avg") {
+			field = "Avg";
+		} else if (field === "pass") {
+			field = "Pass";
+		} else if (field === "fail") {
+			field = "Fail";
+		} else {
+			field = "Audit";
+		}
+		return field;
+	}
+
+	// transform field from naming convention in dataset to naming convention in query
+	public transformDatasetToQueryConvention(field: string): string {
+		if (field === "id") {
+			field = "uuid";
+		} else if (field === "Course") {
+			field = "id";
+		} else if (field === "Title") {
+			field = "title";
+		} else if (field === "Professor") {
+			field = "instructor";
+		} else if (field === "Subject") {
+			field = "dept";
+		} else if (field === "Year") {
+			field = "year";
+		} else if (field === "Avg") {
+			field = "avg";
+		} else if (field === "Pass") {
+			field = "pass";
+		} else if (field === "Fail") {
+			field = "fail";
+		} else {
+			field = "audit";
+		}
+		return field;
+	}
+
+	// returns the expects type for the field (number or string)
+	public returnValueType(field: string) {
+		if (field === "id" || field === "Avg" || field === "Pass" || field === "Fail" || field === "Audit") {
+			return "number";
+		} else {
+			return "string";
+		}
+	}
+}
