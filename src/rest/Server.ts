cc40e479a27d628cb2aee7ee9d671d232f783850
syedwaqarahmed <syedw@student.ubc.ca>
2023-09-08 15:54:58 -0700

    removing cors limitation

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -24,7 +24,7 @@ export default class Server {
 		 * accessible at http://localhost:<port>/
 		 */
 		// this.express.use(express.static("./frontend/public"));
-		this.express.use(cors({origin: "https://ubcdata.onrender.com"}));
+		// this.express.use(cors({origin: "https://ubcdata.onrender.com"}));
 
 
 	}
